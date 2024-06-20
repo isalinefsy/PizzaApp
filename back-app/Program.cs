a5bb3b1de3c45235b8313ccaec9c1dccec2dcb0f
isalinefsy <126682144+isalinefsy@users.noreply.github.com>
2024-06-20 15:17:10 +0200

    Add files via upload

--- back-app/Program.cs
+++ back-app/Program.cs
@@ -1,1 +1,43 @@
+using ContosoPizza.Services;
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<PizzaContext>(options =>
+    options.UseSqlite("Data Source=/home/foissey/Documents/webapi/back-app/pizza.db"));
+builder.Services.AddScoped<PizzaService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:5173")
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowSpecificOrigin");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
